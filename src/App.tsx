cbb8f1bd4a1bd5f5c338608e2eef978a5fb20895
Aryan Ahire <aryanahire9086360780@gmail.com>
2023-04-05 16:27:35 -0400

    made a hosting website using same color pallete

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Flex, Button } from "@chakra-ui/react"
+
+const App = () => {
+  return (
+    <Flex justifyContent="center" alignItems="center" width="100vw" height="100vh">
+      <Button fontSize="6xl" bgColor="green" width="25vw" height="10vh">Hi</Button>
+    </Flex>
+  );
+};
+
+export default App;
