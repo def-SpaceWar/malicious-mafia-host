548e7cb571f9a1975029db978abc1719062f89f2
Aryan Ahire <aryanahire9086360780@gmail.com>
2023-04-15 13:14:30 -0400

    fully functional role assigning

--- src/HostGame.tsx
+++ src/HostGame.tsx
@@ -43,15 +43,57 @@ const HostGame = () => {
     startGame = () => {
       if (lobby.data?.length == 0) return;
 
-      // randomly assign roles here too eventually
+      let
+        villagerCount = 0,
+        guardianCount = 0,
+        mafiaCount = 0,
+        jesterCount = 0;
+
+      gameData.data.map(m => {
+        villagerCount = m.villagerCount;
+        guardianCount = m.guardianCount;
+        mafiaCount = m.mafiaCount;
+        jesterCount = m.jesterCount;
+      });
+
+      const roles: number[] = [];
+      for (let i = 0; i < villagerCount; i++) roles.push(0);
+      for (let i = 0; i < guardianCount; i++) roles.push(1);
+      for (let i = 0; i < mafiaCount; i++) roles.push(2);
+      for (let i = 0; i < jesterCount; i++) roles.push(3);
+
+      const shuffle = (a: any[]) => {
+        for (let i = a.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [a[i], a[j]] = [a[j], a[i]];
+        }
+        return a;
+      }
+
+      shuffle(roles);
+
+      type Role = "villager" | "guardian" | "mafia" | "jester";
+      type Association = "innocent" | "mafia" | "third-party";
+
+      const numberToData = (n: number): {role: Role, association: Association} => {
+        let role: Role = "villager";
+        let association: Association = "innocent";
+
+        if (n == 1) role = "guardian";
+        if (n == 2) role = "mafia", association = "mafia";
+        if (n == 3) role = "jester", association = "third-party";
+
+        return { role, association };
+      };
 
       lobby.data.map((m) => {
+        const identity = numberToData(roles.pop()!);
         setDoc(doc(firestore, "gamePlayers", m.uid), {
           name: m.displayName,
           uid: m.uid,
           email: m.email,
-          role: "villager",
-          association: "innocent"
+          role: identity.role,
+          association: identity.association
         });
       });
     },
@@ -66,7 +108,7 @@ const HostGame = () => {
     <Flex width="100vw" height="100vh">
       {
         (!hasGameStarted())
-          ? <ConfigureGame playerCount={lobby.data.length}/>
+          ? <ConfigureGame playerCount={lobby.data.length} />
           : <RunGame />
       }
       <Flex justifyContent="center" alignItems="center" width="70vw" height="100vh" flexDir="column">
